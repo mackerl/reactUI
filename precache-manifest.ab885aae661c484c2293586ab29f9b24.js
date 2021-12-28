02f265f50d613c8ebd183d279efb1f398ac28dab
Mario Ackerl <mario.ackerl@gmail.com>
2021-12-28 13:34:38 +0100

    Updates

---
+++ precache-manifest.ab885aae661c484c2293586ab29f9b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "055febb9e4c4bc717f84",
+    "url": "/reactUI/static/css/main.6eca4765.chunk.css"
+  },
+  {
+    "revision": "055febb9e4c4bc717f84",
+    "url": "/reactUI/static/js/main.055febb9.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/reactUI/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "e56018d6171619ac8050",
+    "url": "/reactUI/static/js/2.e56018d6.chunk.js"
+  },
+  {
+    "revision": "b0d1a133522208a75377",
+    "url": "/reactUI/static/js/runtime~main.b0d1a133.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reactUI/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1ab2caf910f22039eb256c853dd5fa48",
+    "url": "/reactUI/index.html"
+  }
+];
